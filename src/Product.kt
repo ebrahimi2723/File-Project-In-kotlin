11af86e8ae698ea85df7e58a564548dbebd5c965
ebrahimi2723 <ebrahimi2723@gmail.com>
2022-05-26 11:54:18 +0430

    file with error ArrayIndexOutOfBoundsException

--- src/Product.kt
+++ src/Product.kt
@@ -1,11 +1,27 @@
 import enums.Status
 
 class Product(
-    var title:String,
-    var description:String,
-    var sku:Int,
-    var price:Long,
+    var title: String,
+    var description: String,
+    var sku: Int,
+    var price: Long,
     var status: Status = Status.PREORDER
 
 ) {
+    companion object {
+        fun toProduct(line: String, separator: Char): Product {
+            /////////// convert a line of string to array and convert to product
+            val arr = line.split(separator).toTypedArray()
+
+            val product = Product(
+                arr[0], arr[1], arr[2].toInt(), arr[3].toLong(), when (arr[4]) {
+                    "AVAILABLE" -> Status.AVAILABLE
+                    "UNAVAILABLE" -> Status.UNAVAILABLE
+                    "PREORDER" -> Status.PREORDER
+                    else -> Status.PREORDER
+                }
+            )
+            return product
+        }
+    }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,19 @@
 import java.io.File
 
 fun main() {
-        var productList= mutableListOf<Product>()
+        var listProduct = mutableListOf<Product>()
         File("C:\\\\Users\\a\\IdeaProjects\\File Project In kotlin\\inventory.txt").forEachLine {
-                println(it)
+                ///send a line from file to convert product
+             listProduct.add(Product.toProduct(it,'|'))
+//                toProduct(it,'|')
         }
 
 }
+//fun toProduct(line: String, separator: Char) {
+//        /////////// convert a line of string to array and convert to product
+//        val arr = line.split(separator).toTypedArray()
+//        for (item in arr) {
+//                println("${item.indexOf(item)} is $item")
+//
+//        }
+//}
