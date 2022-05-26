09a9c566aa5a8ef2e4d6c1a1ee1e4bb9237a4cf2
ebrahimi2723 <ebrahimi2723@gmail.com>
2022-05-26 10:54:09 +0430

    file

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,9 @@
+import java.io.File
+
 fun main() {
+        var productList= mutableListOf<Product>()
+        File("C:\\\\Users\\a\\IdeaProjects\\File Project In kotlin\\inventory.txt").forEachLine {
+                println(it)
+        }
 
 }
